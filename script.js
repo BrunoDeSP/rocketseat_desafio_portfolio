89e34798c3e5b3d3eb6f64619a565636fb2816ae
BrunoDeSP <antunesbrunobarbosa1@gmail.com>
2023-04-11 15:39:55 -0300

    navbar added

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const btnNavbar = document.getElementById('btn-menu')
 const btnContacts = document.getElementById('contacts-mobile')
+const navbar = document.getElementById('navbar')
+const Elementfooter = document.getElementsByClassName('rodape')
+
 
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
-    const nav = document.getElementById('navbar')
-    const Elementfooter = document.getElementById('rodape')
-    nav.classList.toggle('active')
-    Elementfooter.classList.toggle('active')
-    event.currentTarget.setAttibute('aria-expanded', 'true')
+    // const Elementfooter = document.getElementsByClassName('rodape')
+    navbar.classList.toggle('active')
+    // Elementfooter.classList.toggle('active')
+    // event.currentTarget.setAttibute('aria-expanded', 'true')
 }
 
 btnNavbar.addEventListener('click', toggleMenu)
